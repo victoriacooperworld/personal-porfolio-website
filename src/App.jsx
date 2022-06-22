064f97a3a6bfa521671a1e6443464eed49c48e45
Victoria Niu <57949035+victoriacooperworld@users.noreply.github.com>
2022-06-22 16:43:57 -0700

    Debugged footer component

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,6 @@ const App = () => {
         <Nav />
         <About />
         <Experience/>
-        {/* <Services/> */}
-        {/* <Portfolio/>
-        <Testimonials/> */}
         <Contact />
         <Footer />
 
